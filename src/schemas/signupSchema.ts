670c5634d9229ae223400e9f723b305954bb9581
bhavil jain <bhaviljain26@gmail.com>
2024-09-01 22:30:19 +0530

    ZOD validation added

--- src/schemas/signupSchema.ts
+++ src/schemas/signupSchema.ts
@@ -5,3 +5,9 @@ export const usernameValidation = z
 .min(2,"username must be alteast 2 characters")
 .max(20,"username must be no more than 20 characters")
 .regex(/^[a-zA-Z0-9_]+$/,"Username must not contain special character")
+
+export const signUpSchema = z.object({
+    username :  usernameValidation,
+    email: z.string().email({message:"Invalid email address"});
+    password: z.string().min(6,{message:"minimum 6 characters"})
+})
